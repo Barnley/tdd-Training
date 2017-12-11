386d184c664cd985d580ed1d720f622b15075fb5
Pascal Kohl <kohl.pascal@googlemail.com>
2017-12-11 20:27:07 +0100

    added csharp part

---
+++ csharp/unittesting/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unittesting
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void test0()
+        {
+            Assert.Pass();
+        }
+    }
+}
